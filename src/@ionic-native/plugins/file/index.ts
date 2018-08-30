aedf72c19bb0d41d5274e5848d522b456d9bcbb8
Adriano Maciel <adriano_mail@hotmail.com>
2018-08-30 16:56:32 -0300

    Adjust getFreeDiskSpace function return
    I've tryed to get the free space, and the return is in KB and noit in Bytes.
    Testes with new version 6.0.1, from https://github.com/apache/cordova-plugin-file/releases

--- src/@ionic-native/plugins/file/index.ts
+++ src/@ionic-native/plugins/file/index.ts
@@ -758,7 +758,7 @@ export class File extends IonicNativePlugin {
 
   /**
    * Get free disk space in Bytes
-   * @returns {Promise<number>} Returns a promise that resolves with the remaining free disk space in Bytes
+   * @returns {Promise<number>} Returns a promise that resolves with the remaining free disk space in Kbytes
    */
   @CordovaCheck()
   getFreeDiskSpace(): Promise<number> {
